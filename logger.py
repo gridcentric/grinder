bec7bf19d1e43621d1226c24b4e47557d16a35eb
Peter Feiner <peter@gridcentric.ca>
2012-06-21 16:03:06 -0400

    Log messages no longer print twice!

--- logger.py
+++ logger.py
@@ -6,5 +6,6 @@ handler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 log = logging.getLogger('openstack-test')
+log.propagate = False
 log.setLevel(logging.DEBUG)
 log.addHandler(handler)
