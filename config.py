628dbd205f3a01c5d235c2448f1f431f51c05554
Peter Feiner <peter@gridcentric.ca>
2012-08-01 10:38:07 -0400

    Refactoring
    This should have been several commits. Not worth splitting it up now :)
    
    Changed all SSH arguments to be server objects instead of IPs.
    
    Compressed simple launch & migration tests into smoke tests. These tests cover
    more stuff, but they run a lot faster because they only boot 1 master.
    
    Moved image config into proper object. No more hacking default_config.
    
    Made image finding abstract.

--- config.py
+++ config.py
@@ -3,30 +3,84 @@ import os
 import hashlib
 
 DEFAULT_TIMEOUT = 60
+SOURCE_DIR = os.path.dirname(inspect.getfile(inspect.currentframe()))
+DEFAULT_KEY = os.path.join(SOURCE_DIR, 'openstack-test.key')
 
 class Config(object):
 
     def __init__(self):
-        # Path to the git repo for openstack-test.
-        self.data_path = os.path.dirname(inspect.getfile(inspect.currentframe()))
-
-        # Hosts to run this test on.
+        # Hosts to run this test on. All should be running OpenStack and have
+        # VMS installed. The versions of OpenStack and VMS should correspond to
+        # vms_version and openstack_version respectively. There should be at
+        # least two.
         self.hosts = ['node10', 'node9']
+        # Hosts without OpenStack installed. There should be at least one.
         self.hosts_without_openstack = ['xdev']
 
+        # Instance flavor; determines RAM and what disks are attached.
         self.flavor_name = 'm1.tiny'
-        self.image_name = 'uec-oneiric-vmsagent3-root'
-        self.guest = "ubuntu"
-        self.guest_has_agent = True
+        # Name of the key to inject into the instances. Only Ubuntu Enterprise
+        # Cloud images support this. Currently, none of our tests rely on an
+        # injected key. Instead, we assume that the Image instances have the
+        # public key corresponding to Image.guest_key_path.
         self.guest_key_name = 'openstack-test'
-        self.guest_key_path = os.path.join(self.data_path, 'openstack-test.key')
-        self.guest_user = 'ubuntu'
+        # Not used.
+        self.guest_key_path = 'Deprecated'
+        # Not used.
+        self.guest_user = 'Deprecated'
+        # The version of openstack running on the hosts. Either "diablo" or
+        # "essex".
         self.openstack_version = "essex"
+        # A username on hosts that has passwordless sudo.
         self.host_user = "tester"
-        self.host_key_path = os.path.join(self.data_path, 'openstack-test.key')
+        # The private SSH key for host_user on all of the hosts.
+        self.host_key_path = os.path.join(SOURCE_DIR, 'openstack-test.key')
+        # How long to wait for servers to come up, become ping-able, ssh-able,
+        # etc. In seconds.
         self.ops_timeout = DEFAULT_TIMEOUT
+        # The ABI version of the agent to install on the master servers. The
+        # latest build of the specified agent is installed on each master.
         self.agent_version = '1'
+        # The version of VMS installed on the cluster.
         self.vms_version = '2.4'
+        # The arch to use for non-arch tests (i.e., tests that aren't sensitive
+        # to the arch).
+        self.default_archs=['64']
+        # The distro to use for non-distro tests (i.e., tests that aren't
+        # sensitive to the distro).
+        self.default_distros=['cirros']
+
+        # The images available in Glance for the tests to use. The tests need
+        # images of certain distributions for certain architectures; hence the
+        # image names aren't important. See Image.
+        self.images = [
+            # Lean & mean cirros image.
+            Image('cirros-0.3.0-x86_64', distro='cirros', arch='64', user='cirros'),
+
+            # Ubuntu images
+            Image('oneiric-agent-ready', distro='ubuntu', arch='64', user='ubuntu'),
+            Image('precise-32bit-agent-ready', distro='ubuntu', arch='32', user='root'),
+            Image('precise-PAE-agent-ready', distro='ubuntu', arch='pae', user='root'),
+            # CentOS images
+            Image('centos-6.3-amd64-agent-ready', distro='centos', arch='64', user='root'),
+            Image('centos-6.3-32bit-agent-ready', distro='centos', arch='32', user='root'),
+            Image('centos-6.3-PAE-agent-ready', distro='centos', arch='pae', user='root'),
+        ]
+
+    def get_all_archs(self):
+        return list(set([i.arch for i in self.images]))
+
+    def get_all_distros(self):
+        return list(set([i.distro for i in self.images]))
+
+    def post_config(self):
+        for image in self.images:
+            if image.key == None:
+                image.key = DEFAULT_KEY
+
+    def get_images(self, distro, arch):
+        return filter(lambda i: i.distro == distro and i.arch == arch,
+                      self.images)
 
     def hostname_to_id(self, tenant_id, hostname):
         if self.openstack_version == "essex":
@@ -48,5 +102,27 @@ class Config(object):
     def parse_vms_version(self):
         (major, minor) = [ int(x) for x in self.vms_version.split('.')[:2] ]
         return (major, minor)
+
+class Image(object):
+    '''Add an image. Fields specified with comma-separated key=val pairs.
+
+    name: the name of the image in glance
+    distro: the linux distribution: use ubuntu, centos, or cirros
+    arch: the architecture -- use 32, 64, or pae
+    user: the user to login as, must have passwordless sudo
+    key: the path to the SSH key for the user; omit to use the default
+
+    e.g., --image 11.10-a1-64,distro=ubuntu,user=ubuntu,arch=64
+      '''
+    def __init__(self, name, distro, arch, user=None, key=None):
+        self.name = name
+        self.distro = distro
+        self.arch = arch
+        self.user = user
+        self.key = key
+
+    def __repr__(self):
+        return 'Image(name=%s, distro=%s, arch=%s, user=%s, key=%s)' % \
+                (self.name, self.distro, self.arch, self.user, self.key)
 
 default_config = Config()
--- conftest.py
+++ conftest.py
@@ -1,5 +1,26 @@
-from config import default_config
+from config import default_config, Image
+
+from harness import ImageFinder, get_test_distros, get_test_archs
+
 import os
+
+def parse_option(value, *default_args, **default_kwargs):
+    '''Parses an option value qemu style: comma-separated, optional keys
+
+    Returns tuple (args, kwargs).
+
+    kwargs - dict from key=value options
+    args - arguments without '='
+    '''
+    args = []
+    kwargs = {}
+    for arg in value.split(','):
+        if '=' in arg:
+            split = arg.split('=', 1)
+            kwargs[split[0]] = split[1]
+        else:
+            args.append(arg)
+    return args, kwargs
 
 def pytest_runtest_setup(item):
     # Can't import harness earlier because pytest screws up importing logger.
@@ -9,17 +30,39 @@ def pytest_runtest_setup(item):
 def pytest_addoption(parser):
     # Add options for each of the default_config fields.
     for name, value in vars(default_config).iteritems():
+        if name == 'images':
+            continue
         if type(value) == list:
             help='default is %s (comma-separated list)' % ','.join(value)
         else:
             help='default is %s' % value
         parser.addoption('--%s' % name, action="store", type="string",
                          default=None, help=help)
+    parser.addoption('--image', action="append", type="string",
+                     help=Image.__doc__, default=[])
 
 def pytest_configure(config):
     for name, value in vars(default_config).iteritems():
+        if name == 'images':
+            continue
         new_value = getattr(config.option, name)
         if new_value != None:
             if type(value) == list:
                 new_value = new_value.split(',')
             setattr(default_config, name, new_value)
+    images = []
+    for image in config.option.image:
+        args, kwargs = parse_option(image)
+        images.append(Image(*args, **kwargs))
+    # Prepend the command-line images to the defaults so the command-line images
+    # take priority
+    images.extend(default_config.images)
+    default_config.images = images
+
+    default_config.post_config()
+
+def pytest_generate_tests(metafunc):
+    if "image_finder" in metafunc.funcargnames:
+        ImageFinder.parametrize(metafunc, 'image_finder',
+                                get_test_distros(metafunc.function),
+                                get_test_archs(metafunc.function))
