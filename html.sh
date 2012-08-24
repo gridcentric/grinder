0faaf403bbbab7a9d059cb5d3a62f8f9a18487ba
Peter Feiner <peter@gridcentric.ca>
2012-08-24 13:57:41 -0400

    Generated index.html for html output.

--- html.sh
+++ html.sh
@@ -7,3 +7,9 @@ for x in logs/*.xml; do
     xsltproc junit2html.xslt $x > $y
     touch --date="$(stat --printf '%y' $x)" $y
 done
+
+echo '<html><body><tt>' > html/index.html
+for x in $(ls -t html | grep '.*\.html$' | grep -v index.html); do
+    echo "$(stat --printf '%y' html/$x | sed 's/\..*//') <a href=\"$x\">$(basename $x .html)</a><br/>" >> html/index.html
+done
+echo '</tt></body></html>' >> html/index.html
