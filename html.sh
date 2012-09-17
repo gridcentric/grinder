088e6ab6777da26e742767f21a3a0af2f288ccf2
Peter Feiner <peter@gridcentric.ca>
2012-09-17 13:45:16 -0400

    Workaround py.test bug in html generation.

--- html.sh
+++ html.sh
@@ -4,10 +4,17 @@ mkdir -p html
 
 for x in logs/*.xml; do
     y=html/$(basename $x .xml).html
-    xsltproc junit2html.xslt $x > $y
-    touch --date="$(stat --printf '%y' $x)" $y
+    if [ ! -e $y -o $x -nt $y ]; then
+        echo "generating $y"
+        xsltproc junit2html.xslt $x > $y
+        # pyunit screws up some xml escaping. It ouputs &amp;gt; when it should
+        # output &gt;. We fix it up with sed here.
+        sed -i 's/&amp;\(gt\|amp\|apos\|quot\);/\&\1;/g' $y
+        touch --date="$(stat --printf '%y' $x)" $y
+    fi
 done
 
+echo "generating html/index.html"
 echo '<html><body><tt>' > html/index.html
 for x in $(ls -t html | grep '.*\.html$' | grep -v index.html); do
     echo "$(stat --printf '%y' html/$x | sed 's/\..*//') <a href=\"$x\">$(basename $x .html)</a><br/>" >> html/index.html
