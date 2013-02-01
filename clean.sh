10a465b725d0ddf2d36d17c5f59c874992a62c8d
Adin Scannell <adin@scannell.ca>
2013-02-01 10:37:13 -0500

    Adding delay time to clean script.
    Change-Id: I9e136b2dad0a09d2b32a675b895f6744fd6f0a10

--- clean.sh
+++ clean.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+stall=0
 for uuid in $(nova list | grep -E '(ACTIVE|ERROR|BUILD)' | awk '{print $2;}'); do
-    nova delete $uuid;
+    nova delete $uuid
+    stall=$(($stall+3))
 done
+sleep $stall
+
+stall=0
 for uuid in $(nova list | grep -E '(BLESSED)' | awk '{print $2;}'); do
-    nova discard $uuid;
+    nova discard $uuid
+    stall=$(($stall+3))
 done
+sleep $stall
