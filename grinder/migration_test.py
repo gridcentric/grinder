97a614c3733fa3e1a46294f1adcd07224a00d7a3
Andres Lagar-Cavilla <andres@lagarcavilla.org>
2014-02-28 15:06:41 -0500

    GRIND-47: Test using hooks to fail a migration
    Change-Id: I0bd2e038c9644af8ec0535a23830102eac1bef53

--- grinder/migration_test.py
+++ grinder/migration_test.py
@@ -18,6 +18,7 @@ import py.test
 
 from . import harness
 from . logger import log
+from . import requirements
 from . util import assert_raises
 from . host import Host
 
@@ -75,3 +76,48 @@ class TestMigration(harness.TestCase):
             master.migrate(dest, host)
             master.migrate(host, dest)
             master.migrate(dest, host)
+
+    # We require host targetting to check hooks availability
+    # *before* booting a guest, thus saving time
+    @harness.requires(requirements.AVAILABILITY_ZONE)
+    def test_migration_rollback(self, image_finder):
+        if self.harness.config.skip_migration_tests:
+            py.test.skip('Skipping migration tests')
+        if len(self.harness.config.hosts) < 2:
+            py.test.skip('Need at least 2 hosts to do migration.')
+        source_host_name    = self.config.hosts[0]
+        source_host         = Host(source_host_name, self.config)
+        if not source_host.check_supports_hooks():
+            py.test.skip("Host %s does not have cobalt hooks enabled." %\
+                         source_host_name)
+        dest_host = Host(self.config.hosts[1], self.harness.config)
+        assert source_host.id != dest_host.id
+        log.debug("Migration rollback source host %s" % source_host_name)
+        log.debug("Migration rollback dest host %s" % dest_host.id)
+        with self.harness.booted(image_finder, agent=False, host=source_host) as master:
+            assert getattr(master, 'id') is not None
+            with source_host.with_hook("01_post_bless", """#!/bin/bash
+set -e
+echo "Post bless hook args: $@" >&2
+[ $# -eq 6 ]
+[ $1 == %s ]
+[ $6 == migration ]
+[[ $5 == mcdist://* ]]
+URL=${5#mcdist://}
+ADDR=$(echo $URL | cut -d '|' -f 1)
+# For good luck
+sleep 3
+[ $(netstat -plun | grep -E 'vmsd\s*$' | awk -v addr="$ADDR" '$4==addr {print $NF}' | wc -l) -eq 1 ]
+PID=$(netstat -plun | grep -E 'vmsd\s*$' | awk -v addr="$ADDR" '$4==addr {print $NF}' | cut -d '/' -f 1)
+kill -9 $PID
+""" % master.id) as hook:
+                # The hook above will kill the memsrv after the bless phase of migration
+                # should be fun...
+                e = assert_raises(AssertionError,
+                          master.migrate,
+                          source_host, dest_host)
+                master.assert_alive(source_host)
+                e = assert_raises(AssertionError,
+                          master.assert_alive,
+                          dest_host)
+
